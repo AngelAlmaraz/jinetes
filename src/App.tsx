4e9055ad1c683107142e1e3eb2e78f1d9c1cbef9
AngelAlmaraz <angel.almaraz@cetys.edu.mx>
2023-05-01 23:18:44 -0700

    ayuda

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import GameGrid from "./components/GameGrid";
 function App() {
   return (
     <>
-      maybe
+      Noooooo
       <Grid
         templateAreas={{
           base: `"nav" "main"`,
