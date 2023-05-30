b30775227d84b98d7bf0be9dedef32f08b8f0ca8
AngelAlmaraz <angel.almaraz@cetys.edu.mx>
2023-05-30 13:57:30 -0700

    NoMeMeoEncima

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -13,74 +13,69 @@ import { BiDownArrow, BiUpArrow } from "react-icons/bi";
 import { Book } from "../hooks/useBooks";
 import { getUser } from "../services/auth";
 import axios from "axios";
-import { useState } from "react";
-
-const user = getUser();
-
-//function for getting kart
-
-const fetchCartData = async (email: string) => {
-  try {
-    const response = await axios.get(
-      "https://4nxi7wtmf2.execute-api.us-east-1.amazonaws.com/dev/getcart",
-      {
-        params: {
-          email: email,
-        },
-      }
-    );
-
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    // Manejar cualquier error que ocurra durante la solicitud
-    throw error; // Lanzar el error para que sea manejado por el llamador de la función
-  }
-};
-
-let kart: Book[] = [];
-if (user) {
-  kart = await fetchCartData(user.email);
-}
+import { useEffect, useState } from "react";
 
 const Cart = () => {
-  if (!user) return <>Error</>;
+  const user = getUser();
+  const [kart, setKart] = useState<Book[]>([]);
 
-  console.log(kart);
+  useEffect(() => {
+    const fetchCartData = async (email: string) => {
+      try {
+        const response = await axios.get(
+          "https://4nxi7wtmf2.execute-api.us-east-1.amazonaws.com/dev/getcart",
+          {
+            params: {
+              email: email,
+            },
+          }
+        );
+
+        setKart(response.data);
+      } catch (error) {
+        console.error(error);
+        // Manejar cualquier error que ocurra durante la solicitud
+      }
+    };
+
+    if (user) {
+      fetchCartData(user.email);
+    }
+  }, [user]);
+
+  if (!user) return <>Error</>;
 
   return (
     <>
       <Box marginX={20}>
-        {kart?.map((book: Book) => (
-          <>
-            <Box key={book.id} margin={5}>
-              <Card padding={7}>
-                <HStack>
-                  <Image
-                    borderRadius={10}
-                    marginX={5}
-                    src={book.url}
-                    width="10%"
-                  />
-                  <VStack paddingX={5}>
-                    <Button>
-                      <BiUpArrow size={20} />
-                    </Button>
-                    <Text>{book.quantity}</Text>
-                    <Button>
-                      <BiDownArrow size={20} />
-                    </Button>
-                  </VStack>
-                  <VStack>
-                    <Heading justifyContent="flex-start" fontSize="2xl">
-                      {book.name}
-                    </Heading>
-                    <Text>{book.description}</Text>
-                  </VStack>
-                </HStack>
-              </Card>
-            </Box>
-          </>
+        {kart.map((book: Book) => (
+          <Box key={book.id} margin={5}>
+            <Card padding={7}>
+              <HStack>
+                <Image
+                  borderRadius={10}
+                  marginX={5}
+                  src={book.url}
+                  width="10%"
+                />
+                <VStack paddingX={5}>
+                  <Button>
+                    <BiUpArrow size={20} />
+                  </Button>
+                  <Text>{book.quantity}</Text>
+                  <Button>
+                    <BiDownArrow size={20} />
+                  </Button>
+                </VStack>
+                <VStack>
+                  <Heading justifyContent="flex-start" fontSize="2xl">
+                    {book.name}
+                  </Heading>
+                  <Text>{book.description}</Text>
+                </VStack>
+              </HStack>
+            </Card>
+          </Box>
         ))}
         <HStack justify="flex-end" paddingX={10}>
           <Button>
